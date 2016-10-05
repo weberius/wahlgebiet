c32d08a6b510fa95b71fd17dca4d5fd5fc9aece2
wolfram <wolfram@wolframs-mbp-3.fritz.box>
2016-10-05 05:30:55 +0200

    don't use database for test

--- src/test/java/de/illilli/opendata/service/wahlgebiet/LoadStimmbezirkFacadeTest.java
+++ src/test/java/de/illilli/opendata/service/wahlgebiet/LoadStimmbezirkFacadeTest.java
@@ -8,22 +8,21 @@ import java.sql.SQLException;
 import javax.naming.NamingException;
 
 import org.junit.Before;
-import org.junit.Test;
 
 import de.illilli.jdbc.ConnectionEnvironment;
 import de.illilli.opendata.service.Facade;
 
 public class LoadStimmbezirkFacadeTest {
 
-	private URL url;
+	private static URL url;
 
 	@Before
 	public void setUp() throws Exception {
 		url = new File("./src/main/resources/stimmbezirk/Stimmbezirk.shp").toURI().toURL();
 	}
 
-	@Test
-	public void test() throws IOException, SQLException, NamingException {
+	public static void main(String[] args) throws IOException, SQLException, NamingException {
+		url = new File("./src/main/resources/stimmbezirk/Stimmbezirk.shp").toURI().toURL();
 		ConnectionEnvironment.setUpConnectionForJndi();
 		Facade facade = new LoadStimmbezirkFacade(url);
 		System.out.println(facade.getJson());
