296edb81f5056de16feee0f254e553a2ca9e99e5
wolfram <wolfram@wolframs-mbp-3.fritz.box>
2016-11-07 18:41:23 +0100

    Spalten shape_area und shape_len befüllen #10

--- src/main/sql/wahlgebiet.init.sql
+++ src/main/sql/wahlgebiet.init.sql
@@ -8,8 +8,6 @@ CREATE TABLE stimmbezirk (
     STB          varchar(256),
     NR_STT       integer,
     STT          varchar(256),
-    SHAPE_AREA   double precision,
-    SHAPE_LEN    double precision,
     modtime      timestamp DEFAULT current_timestamp
 );
 SELECT AddGeometryColumn ('public','stimmbezirk','geom',4326,'MULTIPOLYGON',2);
